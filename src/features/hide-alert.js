eeac25834e4fefe6c56fd645c56c7f991cfea40c
Zyk <basesukid@gmail.com>
2026-08-16 20:48:17 +0200

    Implement hideAlert feature to hide alerts

--- src/features/hide-alert.js
+++ src/features/hide-alert.js
@@ -1,1 +1,19 @@
+(function() {
+    'use strict';
 
+    function apply() {
+        var alert = document.querySelector('[class*="alertBg-"]');
+        if (alert) {
+            alert.style.display = 'none';
+        } else {
+            setTimeout(apply, 400);
+        }
+    }
+
+    window.NX = window.NX || {};
+    window.NX.features = window.NX.features || {};
+    window.NX.features.hideAlert = {
+        apply: apply
+    };
+
+})();
