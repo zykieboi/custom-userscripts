926c5ecfa6ce46fafb66750944ce0a81f6f89e4e
Zyk <basesukid@gmail.com>
2026-08-16 20:49:30 +0200

    Implement ad removal feature with CSS styles

--- src/features/remove-ads.js
+++ src/features/remove-ads.js
@@ -1,1 +1,32 @@
+(function() {
+    'use strict';
 
+    var adStyleAdded = false;
+
+    function apply() {
+        if (adStyleAdded) return;
+
+        var style = document.createElement('style');
+        style.textContent = `
+            [class*="adWrapper"],
+            [class*="ad-"],
+            [class*="Ad"],
+            .adWrapper-0-2-7,
+            .adWrapper-0-2-11,
+            .adWrapper-0-2-39,
+            .adWrapper-0-2-47,
+            .adWrapper-0-2-163 {
+                display: none !important;
+            }
+        `;
+        document.head.appendChild(style);
+        adStyleAdded = true;
+    }
+
+    window.NX = window.NX || {};
+    window.NX.features = window.NX.features || {};
+    window.NX.features.removeAds = {
+        apply: apply
+    };
+
+})();
