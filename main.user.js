22f98844518721e35f68996044ee2545e30f68dc
Zyk <basesukid@gmail.com>
2026-09-13 18:29:52 +0200

    Add trade-2020 feature requirement
    Added a new requirement for the trade-2020 feature.

--- main.user.js
+++ main.user.js
@@ -19,6 +19,7 @@
 // @require      https://raw.githubusercontent.com/zykieboi/custom-userscripts/main/src/features/inventory-search.js
 // @require      https://raw.githubusercontent.com/zykieboi/custom-userscripts/main/src/features/bulk-unfriend.js
 // @require      https://raw.githubusercontent.com/zykieboi/custom-userscripts/main/src/ui/modal.js
+// @require      https://raw.githubusercontent.com/zykieboi/custom-userscripts/main/src/features/trade-2020.js
 // @downloadURL  https://raw.githubusercontent.com/zykieboi/custom-userscripts/main/main.user.js
 // @updateURL    https://raw.githubusercontent.com/zykieboi/custom-userscripts/main/main.user.js
 // ==/UserScript==
