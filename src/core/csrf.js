f213470ba4b2a539ec905c92b179a071d332af12
Zyk <basesukid@gmail.com>
2026-09-13 20:23:25 +0200

    Refactor CSRF handling for XMLHttpRequest and fetch

--- src/core/csrf.js
+++ src/core/csrf.js
@@ -1,43 +1,39 @@
 (function() {
     'use strict';
 
-    window.NX_CSRF = GM_getValue('nx_csrf', '');
+    window.NX_CSRF = window.NX_CSRF || GM_getValue('nx_csrf', '') || '';
 
-    var originalOpen = XMLHttpRequest.prototype.open;
-    var originalSetHeader = XMLHttpRequest.prototype.setRequestHeader;
+    var origFetch = window.fetch;
+    window.fetch = function(input, init) {
+        var p = origFetch.apply(this, arguments);
+        try {
+            p.then(function(res) {
+                var t = res && res.headers && res.headers.get('x-csrf-token');
+                if (t) {
+                    window.NX_CSRF = t;
+                    GM_setValue('nx_csrf', t);
+                }
+            }).catch(function() {});
+        } catch (e) {}
+        return p;
+    };
+
+    var origOpen = XMLHttpRequest.prototype.open;
+    var origSend = XMLHttpRequest.prototype.send;
 
     XMLHttpRequest.prototype.open = function(method, url) {
         this._nxUrl = url;
-        return originalOpen.apply(this, arguments);
-    };
-
-    XMLHttpRequest.prototype.setRequestHeader = function(header, value) {
-        if (header.toLowerCase() === 'x-csrf-token' && value) {
-            window.NX_CSRF = value;
-            GM_setValue('nx_csrf', value);
-        }
-        return originalSetHeader.apply(this, arguments);
-    };
-
-    var originalFetch = window.fetch;
-    window.fetch = function(url, opts) {
-        if (opts && opts.headers) {
-            var headers = opts.headers;
-            if (headers instanceof Headers) {
-                var token = headers.get('X-CSRF-Token');
-                if (token) {
-                    window.NX_CSRF = token;
-                    GM_setValue('nx_csrf', token);
+        this.addEventListener('load', function() {
+            try {
+                var t = this.getResponseHeader && this.getResponseHeader('x-csrf-token');
+                if (t) {
+                    window.NX_CSRF = t;
+                    GM_setValue('nx_csrf', t);
                 }
-            } else if (typeof headers === 'object') {
-                var token = headers['X-CSRF-Token'] || headers['x-csrf-token'];
-                if (token) {
-                    window.NX_CSRF = token;
-                    GM_setValue('nx_csrf', token);
-                }
-            }
-        }
-        return originalFetch.apply(this, arguments);
+            } catch (e) {}
+        });
+        return origOpen.apply(this, arguments);
     };
 
+    XMLHttpRequest.prototype.send = origSend;
 })();
